041c854b0a11de24c676a13322fcaf9eb34ac45e
Valentina <matsulevichvalentina@gmail.com>
2022-07-15 17:29:51 +0300

    add message "need to login"

--- public/scripts/upload_image.js
+++ public/scripts/upload_image.js
@@ -1,8 +1,13 @@
-addImagePet.addEventListener('click', async () => {
-    if (!localStorage.token) {
-        alert('Need to login')
-    }
-})
+console.log('token', localStorage.token);
+
+if (!localStorage.token) {
+    const containerUpload = document.querySelector('.containerUpload')
+
+    let div = document.createElement('div')
+    div.className = "warning"
+    div.innerHTML = "<strong>Need to login!</strong>"
+    containerUpload.replaceWith(div)
+}
 
 upload.addEventListener('click', async (e) => {
     if (!nameImage.value) {
