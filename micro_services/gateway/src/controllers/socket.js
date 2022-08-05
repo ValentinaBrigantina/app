1928a7d4ff250f118a6b2a403a75eebf43f8b991
Valentina <matsulevichvalentina@gmail.com>
2022-08-05 16:26:05 +0300

    add new format date

--- micro_services/gateway/src/controllers/socket.js
+++ micro_services/gateway/src/controllers/socket.js
@@ -1,4 +1,5 @@
 const socketModel = require('../models/socket')
+const { parsingDate } = require('../utils/date-parsing')
 
 const addMessageToJson = async (data) => {
     const result = await socketModel.addNewMessage(data)
@@ -6,9 +7,10 @@ const addMessageToJson = async (data) => {
 
 exports.socketHandlers = {
     async onChatMessage(socket, data) {
+        data.date = parsingDate(data.date)
         await addMessageToJson(data)
         return data
-    },
+    }
 }
 
 exports.renderChat = (req, res)=> {
