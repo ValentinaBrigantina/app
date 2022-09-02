dbcd860e1f820d82c8c6653ec0507c7be82a85e8
Valentina <matsulevichvalentina@gmail.com>
2022-09-02 18:34:04 +0300

    fix nav_mobile

--- micro_services/gateway/public/scripts/index.js
+++ micro_services/gateway/public/scripts/index.js
@@ -24,13 +24,30 @@ const renderAvatar = (avatar, name) => (
         `
 )
 
-const renderingNav = async () => {
-    const btnAddImage = document.getElementById('btnAddImage')
-    const btnChat = document.getElementById('btnChat')
-    const btnSignUp = document.getElementById('btnSignUp')
-    const btnSignIn = document.getElementById('btnSignIn')
-    const btnMyProfile = document.getElementById('btnMyProfile')
+const btnAddImage = document.getElementById('btnAddImage')
+const btnChat = document.getElementById('btnChat')
+const btnSignUp = document.getElementById('btnSignUp')
+const btnSignIn = document.getElementById('btnSignIn')
+const btnMyProfile = document.getElementById('btnMyProfile')
+const btnAddImageMobile = document.querySelector('.btnAddImageMobile')
+const btnChatMobile = document.querySelector('.btnChatMobile')
+const btnSignUpMobile = document.querySelector('.btnSignUpMobile')
+const btnSignInMobile = document.querySelector('.btnSignInMobile')
+const btnMyProfileMobile = document.querySelector('.btnMyProfileMobile')
+const signOut = document.querySelector('.signOut')
+const signOutMobile = document.querySelector('.signOutMobile')
 
+signOut.addEventListener('click', () => {
+    localStorage.removeItem('token')
+    console.log('remove token', localStorage.token);
+ })
+
+ signOutMobile.addEventListener('click', () => {
+    localStorage.removeItem('token')
+    console.log('remove token mobile', localStorage.token);
+ })
+
+async function renderingNav() {
     const dataUser = await checkToken()
 
     if (localStorage.token) {
@@ -47,16 +64,29 @@ const renderingNav = async () => {
     }
 }
 
+const renderingNavMobile = async () => {
+    const dataUser = await checkToken()
+
+    if (localStorage.token) {
+        btnSignUpMobile.remove()
+        btnSignInMobile.remove()
+        const image = dataUser.image ? dataUser.image : "images/service/ext.png"
+        const avatar = renderAvatar(image, dataUser.name)
+        btnAddImageMobile.insertAdjacentHTML('afterbegin', avatar)
+    } else {
+        btnAddImageMobile.remove()
+        btnChatMobile.remove()
+        btnMyProfileMobile.remove()
+    }
+}
+
 const onloadHandler = async () => {
     await renderingNav()
+    await renderingNavMobile()
     const sidenavs = document.querySelectorAll('.sidenav')
     const dropdown = document.querySelector('.dropdown-trigger')
     M.Sidenav.init(sidenavs)
     M.Dropdown.init(dropdown)
-
-    signOut.addEventListener('click', async () => {
-       localStorage.removeItem('token')
-    })
 }
 
 document.addEventListener("DOMContentLoaded", onloadHandler)
